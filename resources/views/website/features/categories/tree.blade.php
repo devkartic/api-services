6c8813b517845de6253839ed5a57b4327fdb7839
Kartic Gharami <kartic@mvisolutions.com>
2023-09-25 14:35:04 +0600

    Working with tree view style with js

--- resources/views/website/features/categories/tree.blade.php
+++ resources/views/website/features/categories/tree.blade.php
@@ -1,5 +1,79 @@
 @extends('website.app')
 @section('content')
+    <style>
+        .tree, .tree ul {
+            margin: 0;
+            padding: 0;
+            list-style: none
+        }
+
+        .tree ul {
+            margin-left: 1em;
+            position: relative
+        }
+
+        .tree ul ul {
+            margin-left: .5em
+        }
+
+        .tree ul:before {
+            content: "";
+            display: block;
+            width: 0;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            border-left: 1px solid
+        }
+
+        .tree li {
+            margin: 0;
+            padding: 0 1em;
+            line-height: 2em;
+            color: #369;
+            font-weight: 700;
+            position: relative
+        }
+
+        .tree ul li:before {
+            content: "";
+            display: block;
+            width: 10px;
+            height: 0;
+            border-top: 1px solid;
+            margin-top: -1px;
+            position: absolute;
+            top: 1em;
+            left: 0
+        }
+
+        .tree ul li:last-child:before {
+            background: #fff;
+            height: auto;
+            top: 1em;
+            bottom: 0
+        }
+
+        .indicator {
+            margin-right: 5px;
+        }
+
+        .tree li a {
+            text-decoration: none;
+            color: #369;
+        }
+
+        .tree li button, .tree li button:active, .tree li button:focus {
+            text-decoration: none;
+            color: #369;
+            border: none;
+            background: transparent;
+            margin: 0px 0px 0px 0px;
+            padding: 0px 0px 0px 0px;
+            outline: 0;
+        }
+    </style>
     <div class="container">
         <div class="row cs-pb-30">
             <div class="col-md-6 cs-mt-30">
@@ -8,38 +82,91 @@
                         Categories tree view
                     </div>
                     <div class="card-body">
-                        <ul class="list-group">
+                        <ul id="cs_tree">
+                            <li><a href="#">Categories</a>
                             @php
-                                function displayTree($elements, $level = 0):void {
+                                function displayTree($elements, $parent_id=0, $current_level = 0, $previous_level = -1):void {
                                     foreach ($elements as $element) {
-                                        $hasChildren = $element->children->isNotEmpty();
-                                        if ($hasChildren) {
-                                            echo '<li class="list-group-item">';
-                                            echo '<button class="btn btn-collapse btn-sm btn-primary mb-2 text-capitalize" data-toggle="collapse" href="#"><i class="fa fa-plus"></i> '.$element->name.'</button>';
-                                            echo '<div class="collapse"><ul class="list-group">';
-                                            displayTree($element->children, $level + 1);
-                                        } else {
-                                            echo '<li class="list-group-item text-capitalize">'.$element->name.'</li>';
+                                        if($current_level > $previous_level){
+                                            echo '<ul>';
                                         }
+                                        if($current_level == $previous_level) {
+                                            echo '</li>';
+                                        }
+                                        echo '<li>'.$element->name;
+                                        if($current_level > $previous_level) {
+                                            $previous_level = $current_level;
+                                        }
+                                        $current_level++;
+                                        displayTree($element->children, $element->id, $current_level, $previous_level);
+                                        $current_level--;
                                     }
-                                    echo '</ul></div></li>';
+                                    if($current_level == $previous_level) {
+                                        echo '</li></ul>';
+                                    }
                                 }
                                 // Display the tree
                                 displayTree($tree);
                             @endphp
                             <script>
-                                $(document).ready(function () {
-                                    // Enable Bootstrap collapse functionality
-                                    $('[data-toggle="collapse"]').collapse();
+                                $.fn.extend({
+                                    treed: function (o) {
+                                        var openedClass = 'fa-minus';
+                                        var closedClass = 'fa-plus';
 
-                                    // Handle the click event on parent elements
-                                    $('.list-group-item button').on('click', function (e) {
-                                        e.preventDefault(); // Prevent the default behavior of the anchor link
-                                        $(this).find('i').toggleClass('fa-plus').toggleClass('fa-minus');
-                                        $(this).next('.collapse').collapse('toggle'); // Toggle the collapse state of the next element
-                                    });
+                                        if (typeof o != 'undefined') {
+                                            if (typeof o.openedClass != 'undefined') {
+                                                openedClass = o.openedClass;
+                                            }
+                                            if (typeof o.closedClass != 'undefined') {
+                                                closedClass = o.closedClass;
+                                            }
+                                        }
+                                        ;
+
+                                        //initialize each of the top levels
+                                        var tree = $(this);
+                                        tree.addClass("tree");
+                                        tree.find('li').has("ul").each(function () {
+                                            var branch = $(this); //li with children ul
+                                            branch.prepend("<i class='fa " + closedClass + "'></i> ");
+                                            branch.addClass('branch');
+                                            branch.on('click', function (e) {
+                                                if (this == e.target) {
+                                                    var icon = $(this).children('i:first');
+                                                    icon.toggleClass(openedClass + " " + closedClass);
+                                                    $(this).children().children().toggle();
+                                                }
+                                            })
+                                            branch.children().children().toggle();
+                                        });
+                                        //fire event from the dynamically added icon
+                                        tree.find('.branch .indicator').each(function () {
+                                            $(this).on('click', function () {
+                                                $(this).closest('li').click();
+                                            });
+                                        });
+                                        //fire event to open branch if the li contains an anchor instead of text
+                                        tree.find('.branch > a').each(function () {
+                                            $(this).on('click', function (e) {
+                                                $(this).closest('li').click();
+                                                e.preventDefault();
+                                            });
+                                        });
+                                        //fire event to open branch if the li contains a button instead of text
+                                        tree.find('.branch > button').each(function () {
+                                            $(this).on('click', function (e) {
+                                                $(this).closest('li').click();
+                                                e.preventDefault();
+                                            });
+                                        });
+                                    }
                                 });
+
+                                //Initialization of tree views
+                                $('#cs_tree').treed();
                             </script>
+                            </li>
                         </ul>
                     </div>
                 </div>
